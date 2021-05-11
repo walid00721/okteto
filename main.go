a91477e25751a61f77539ebe96400c7feb6a8345
Anonymos <email@example.com>
2021-05-11 12:14:24 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    fmt.Println("Starting hello-world server...")
+    http.HandleFunc("/", helloServer)
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        panic(err)
+    }
+}
